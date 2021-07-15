f00524c596acfac097552d6547b64ab100888a61
Stephen Toms <s.mark.toms@gmail.com>
2021-07-15 01:56:48 -0400

    added in loading list indicators

--- resources/js/vue/pages/Sites/Index.vue
+++ resources/js/vue/pages/Sites/Index.vue
@@ -53,44 +53,99 @@
                             </WButtonsBase>
                         </div>
                         <div class="h-96 overflow-auto pr-5">
-                            <div
-                                v-for="(website, index) in websites"
-                                :key="index"
-                                class="
-                                    flex
-                                    space-x-6
-                                    rounded-lg
-                                    cursor-pointer
-                                    hover:bg-gray-100
-                                    p-2
-                                "
-                                @click="
-                                    $router.push(`/sites/view/${website.id}`)
-                                "
-                            >
+                            <div v-if="websites && websites.length > 0">
                                 <div
-                                    class="w-24 h-20 rounded-lg bg-gray-200"
-                                ></div>
-                                <div
-                                    class="my-auto flex w-full justify-between"
+                                    v-for="(website, index) in websites"
+                                    :key="index"
+                                    class="
+                                        flex
+                                        space-x-6
+                                        rounded-lg
+                                        cursor-pointer
+                                        hover:bg-gray-100
+                                        p-2
+                                    "
+                                    @click="
+                                        $router.push(
+                                            `/sites/view/${website.id}`
+                                        )
+                                    "
                                 >
-                                    <div>
-                                        <div class="font-bold">
-                                            {{ website.name }}
+                                    <div
+                                        class="w-24 h-20 rounded-lg bg-gray-200"
+                                    ></div>
+                                    <div
+                                        class="
+                                            my-auto
+                                            flex
+                                            w-full
+                                            justify-between
+                                        "
+                                    >
+                                        <div>
+                                            <div class="font-bold">
+                                                {{ website.name }}
+                                            </div>
+                                            <div class="text-sm text-gray-500">
+                                                {{ website.domain }}
+                                            </div>
                                         </div>
-                                        <div class="text-sm text-gray-500">
-                                            {{ website.domain }}
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="font-medium">
-                                            {{ website.created_at }}
-                                        </div>
-                                        <div class="text-sm text-gray-500">
-                                            {{ website.updated_at }}
+                                        <div>
+                                            <div class="font-medium">
+                                                {{ website.created_at }}
+                                            </div>
+                                            <div class="text-sm text-gray-500">
+                                                {{ website.updated_at }}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div v-else-if="websites" class="h-full flex">
+                                <div class="text-center mx-auto my-auto">
+                                    <svg
+                                        class="mx-auto h-12 w-12 text-gray-400"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            vector-effect="non-scaling-stroke"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+                                        />
+                                    </svg>
+                                    <h3
+                                        class="
+                                            mt-2
+                                            text-sm
+                                            font-medium
+                                            text-gray-900
+                                        "
+                                    >
+                                        No websites
+                                    </h3>
+                                    <p class="mt-1 text-sm text-gray-500">
+                                        Get started by creating a new website.
+                                    </p>
+                                    <div class="mt-6">
+                                        <WButtonsBase
+                                            icon="plus"
+                                            class="mx-auto"
+                                            @click="
+                                                $router.push('/sites/create')
+                                            "
+                                        >
+                                            Get started
+                                        </WButtonsBase>
+                                    </div>
+                                </div>
+                            </div>
+                            <div v-else>
+                                <LoadingList />
                             </div>
                         </div>
                     </div>
@@ -102,28 +157,28 @@
 
 <script>
 import ContainedLayout from '../../layouts/ContainedLayout.vue'
-import WButtonsBase from '../../components/buttons/Base.vue'
+import LoadingList from '../../components/loading/List.vue'
 export default {
     name: 'Dashboard',
     components: {
         ContainedLayout,
-        WButtonsBase,
+        LoadingList,
     },
     data() {
         return {
             websiteName: '',
-            websites: [],
+            websites: null,
         }
     },
-    created() {
+    async mounted() {
         if (window.Laravel.user) {
             this.name = window.Laravel.user.name
         }
-        this.getWebsites()
+        await this.getWebsites()
     },
     methods: {
-        getWebsites() {
-            this.$axios
+        async getWebsites() {
+            await this.$axios
                 .post('graphql', {
                     query: `
                     {
