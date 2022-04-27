20d781ffbd6b87be6377f2c4d3d2f31b99c60859
Stephen Toms <s.mark.toms@gmail.com>
2022-04-27 16:23:06 -0400

    started on edit view

--- resources/js/vue/pages/Sites/Edit.vue
+++ resources/js/vue/pages/Sites/Edit.vue
@@ -4,129 +4,111 @@
             <PrimaryNav />
         </template>
         <template #content>
-            <div class="space-y-2">
-                <div class="flex space-x-2">
-                    <div class="w-full bg-white shadow p-5 rounded-lg">
-                        <div class="flex justify-between border-b -2 pb-2 mb-4">
-                            <div class="flex space-x-2">
-                                <div class="w-56">
+            <div v-if="website" class="space-y-2 max-w-3xl mx-auto">
+                <div>
+                    <router-link
+                        to="/sites"
+                        class="text-primary-200 font-semibold flex"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-6 w-6 mr-2"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M7 16l-4-4m0 0l4-4m-4 4h18"
+                            />
+                        </svg>
+                        Back to sites
+                    </router-link>
+                    <Form @submit="onSubmit">
+                        <div class="mt-3 w-full bg-white shadow rounded-lg">
+                            <div class="flex justify-between pb-2 mb-2 p-5">
+                                <div class="text-2xl font-medium secondary-600">
+                                    Editing:
+                                    <span class="text-gray-500 italic">{{
+                                        website.name
+                                    }}</span>
+                                </div>
+                            </div>
+                            <div class="bg-gray-200 p-3 px-5 flex">
+                                <div class="w-1/2">Name:</div>
+                                <div class="w-1/2">
                                     <Field
-                                        v-model="websiteName"
+                                        v-model="website.name"
                                         name="name"
-                                        placeholder="Search"
+                                        :rules="isRequired"
                                         class="h-10 text-primary-700 appearance-none w-full px-3 py-2 border border-gray-300 rounded-md placeholder-primary-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                                        @input="
-                                            websiteName === '' && getWebsites()
-                                        "
+                                    />
+                                    <ErrorMessage
+                                        name="domain"
+                                        class="text-red-600 text-sm"
                                     />
                                 </div>
-                                <WButtonsBase
-                                    v-if="websiteName !== ''"
-                                    icon="plus"
-                                    @click="getWebsites()"
-                                >
-                                    Search
-                                </WButtonsBase>
                             </div>
-                            <WButtonsBase
-                                icon="plus"
-                                @click="$router.push('/sites/create')"
-                            >
-                                Create new
+                            <div class="p-3 px-5 flex">
+                                <div class="w-1/2">Domain:</div>
+                                <div class="w-1/2">
+                                    <Field
+                                        v-model="website.domain"
+                                        name="domain"
+                                        :rules="isRequired"
+                                        class="h-10 text-primary-700 appearance-none w-full px-3 py-2 border border-gray-300 rounded-md placeholder-primary-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                                    />
+                                    <ErrorMessage
+                                        name="domain"
+                                        class="text-red-600 text-sm"
+                                    />
+                                </div>
+                            </div>
+                            <div class="bg-gray-200 p-3 px-5 flex">
+                                <div class="w-1/2">Netlify Build Hook:</div>
+                                <div class="w-1/2">
+                                    <Field
+                                        v-model="website.netlify_build_hook"
+                                        name="netlify_build_hook"
+                                        :rules="isRequired"
+                                        class="h-10 text-primary-700 appearance-none w-full px-3 py-2 border border-gray-300 rounded-md placeholder-primary-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                                    />
+                                    <ErrorMessage
+                                        name="netlify_build_hook"
+                                        class="text-red-600 text-sm"
+                                    />
+                                </div>
+                            </div>
+                            <div class="p-3 px-5 flex">
+                                <div class="w-1/2">Created:</div>
+                                <div class="w-1/2">
+                                    {{
+                                        $dayjs(website.created_at).format(
+                                            'MMM DD, YYYY'
+                                        )
+                                    }}
+                                </div>
+                            </div>
+                            <div class="bg-gray-200 p-3 px-5 flex">
+                                <div class="w-1/2">Last Updated:</div>
+                                <div class="w-1/2">
+                                    {{
+                                        $dayjs(website.updated_at).format(
+                                            'MMM DD, YYYY'
+                                        )
+                                    }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-4">
+                            <WButtonsBase icon="plus" class="w-36 ml-auto">
+                                <div v-if="!submitting">Update</div>
+                                <div v-else>Updating...</div>
                             </WButtonsBase>
                         </div>
-                        <div class="h-96 overflow-auto pr-5">
-                            <div v-if="websites && websites.length > 0">
-                                <div
-                                    v-for="(website, index) in websites"
-                                    :key="index"
-                                    class="flex space-x-6 rounded-lg cursor-pointer hover:bg-gray-100 p-2"
-                                    @click="
-                                        $router.push(
-                                            `/sites/view/${website.id}`
-                                        )
-                                    "
-                                >
-                                    <div
-                                        class="w-24 h-20 rounded-lg bg-gray-200"
-                                    ></div>
-                                    <div
-                                        class="my-auto flex w-full justify-between"
-                                    >
-                                        <div>
-                                            <div class="font-bold">
-                                                {{ website.name }}
-                                            </div>
-                                            <div class="text-sm text-gray-500">
-                                                {{ website.domain }}
-                                            </div>
-                                        </div>
-                                        <div class="justify-end flex">
-                                            <div class="w-48 text-right">
-                                                <div class="font-medium">
-                                                    {{
-                                                        $dayjs(
-                                                            website.created_at
-                                                        ).format('MMM DD, YYYY')
-                                                    }}
-                                                </div>
-                                                <div
-                                                    class="text-sm text-gray-500"
-                                                >
-                                                    {{
-                                                        $dayjs(
-                                                            website.updated_at
-                                                        ).format('MMM DD, YYYY')
-                                                    }}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div v-else-if="websites" class="h-full flex">
-                                <div class="text-center mx-auto my-auto">
-                                    <svg
-                                        class="mx-auto h-12 w-12 text-gray-400"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        aria-hidden="true"
-                                    >
-                                        <path
-                                            vector-effect="non-scaling-stroke"
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-                                        />
-                                    </svg>
-                                    <h3
-                                        class="mt-2 text-sm font-medium text-gray-900"
-                                    >
-                                        No websites
-                                    </h3>
-                                    <p class="mt-1 text-sm text-gray-500">
-                                        Get started by creating a new website.
-                                    </p>
-                                    <div class="mt-6">
-                                        <WButtonsBase
-                                            icon="plus"
-                                            class="mx-auto"
-                                            @click="
-                                                $router.push('/sites/create')
-                                            "
-                                        >
-                                            Get started
-                                        </WButtonsBase>
-                                    </div>
-                                </div>
-                            </div>
-                            <div v-else>
-                                <LoadingList />
-                            </div>
-                        </div>
-                    </div>
+                    </Form>
                 </div>
             </div>
         </template>
@@ -135,49 +117,64 @@
 
 <script>
 import ContainedLayout from '../../layouts/ContainedLayout.vue'
-import LoadingList from '../../components/loading/List.vue'
+
 export default {
     name: 'Dashboard',
     components: {
         ContainedLayout,
-        LoadingList,
     },
     data() {
         return {
-            websiteName: '',
-            websites: null,
+            website: null,
+            submitting: false,
         }
     },
-    async mounted() {
+    created() {
         if (window.Laravel.user) {
             this.name = window.Laravel.user.name
         }
-        await this.getWebsites()
+        this.$axios
+            .post('/graphql', {
+                query: `
+                    {
+                        website(id: ${this.$route.params.id}){
+                            name
+                            domain
+                            netlify_build_hook
+                            id
+                            created_at
+                            updated_at
+                        }
+                    }
+                    `,
+            })
+            .then((res) => {
+                this.website = res.data.data.website
+            })
     },
     methods: {
-        async getWebsites() {
+        isRequired(value) {
+            return value ? true : 'This field is required'
+        },
+        async onSubmit(values) {
+            let query = JSON.stringify(values)
+                .replace(/[{}]/g, '')
+                .replace(/"([^"]+)":/g, '$1:')
+            this.submitting = true
             await this.$axios
-                .post('graphql', {
+                .post('/graphql', {
                     query: `
-                    {
-                        websites(first: 6, where: { column: NAME, operator: LIKE, value: "%${this.websiteName}%" }) {
-                            data{
-                                domain
-                                name
-                                id
-                                created_at
-                                updated_at
-                            }
-                            paginatorInfo {
-                                currentPage
-                                lastPage
-                            }
+                    mutation {
+                        createWebsite(${query}) {
+                            id
                         }
                     }
                     `,
                 })
-                .then((result) => {
-                    this.websites = result.data.data.websites.data
+                .then((res) => {
+                    this.$router.push(
+                        `/sites/view/${res.data.data.createWebsite.id}`
+                    )
                 })
         },
     },
