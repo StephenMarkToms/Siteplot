1ca09030673fdeb6c6c49ada7d322623d1a76798
Stephen Toms <s.mark.toms@gmail.com>
2022-04-27 12:16:26 -0400

    upgraded tailwind versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: {
             colors: {
