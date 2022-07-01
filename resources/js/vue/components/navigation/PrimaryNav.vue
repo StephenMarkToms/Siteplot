2bca63887d22b7c6aa55ad15ba8ba37c4a9bee34
Stephen Toms <s.mark.toms@gmail.com>
2022-07-01 01:09:46 -0400

    updated user icon

--- resources/js/vue/components/navigation/PrimaryNav.vue
+++ resources/js/vue/components/navigation/PrimaryNav.vue
@@ -68,11 +68,20 @@
                                     aria-haspopup="true"
                                 >
                                     <span class="sr-only">Open user menu</span>
-                                    <img
-                                        class="h-8 w-8 rounded-full"
-                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                        alt=""
-                                    />
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-8 w-8 rounded-full text-gray-600"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        />
+                                    </svg>
                                 </button>
                             </div>
                         </div>
